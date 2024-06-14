238d3e89779d231f3e4f3ec6b61100f4ced166e3
mariaaa <108476641+mariiia1@users.noreply.github.com>
2024-06-14 11:31:17 -0300

    edit

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = documento.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoPerguntas = document.querySelector(".texto-resultado");
+
+const = perguntas[
+    {
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+    ]
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        }
+        ]
+        
+    
+]
